15536f06741704ae8846c1cc5f01e91340bf9059
dwu45 <david.s.wu@gatech.edu>
2016-04-05 09:24:13 -0700

    make service applicationContextAware

--- src/main/java/edu/gatech/projectThree/service/Scheduler.java
+++ src/main/java/edu/gatech/projectThree/service/Scheduler.java
@@ -1,17 +1,18 @@
 package edu.gatech.projectThree.service;
 
-import edu.gatech.projectThree.datamodel.dao.impl.CourseDAO;
-import edu.gatech.projectThree.datamodel.dao.impl.OfferingDAO;
-import edu.gatech.projectThree.datamodel.dao.impl.ProfessorDAO;
-import edu.gatech.projectThree.datamodel.dao.impl.StudentDAO;
+import edu.gatech.projectThree.datamodel.dao.impl.*;
 import edu.gatech.projectThree.datamodel.entity.Course;
 import edu.gatech.projectThree.datamodel.entity.Professor;
+import edu.gatech.projectThree.datamodel.entity.Semester;
 import edu.gatech.projectThree.datamodel.entity.Student;
 import edu.gatech.projectThree.service.Constraint.Constraint;
 import org.aspectj.weaver.ast.Var;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import gurobi.*;
 
@@ -22,7 +23,7 @@ import java.util.Collection;
  * Created by dawu on 3/18/16.
  */
 @Service("scheduler")
-public class Scheduler {
+public class Scheduler implements ApplicationContextAware {
 
     @Autowired
     @Qualifier("studentDAO")
@@ -40,23 +41,45 @@ public class Scheduler {
     @Qualifier("offeringDAO")
     OfferingDAO offeringDAO;
 
+    @Autowired
+    @Qualifier("semesterDAO")
+    SemesterDAO semesterDAO;
+
     private ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
 
     public double schedule() {
         double result = 0;
         try {
             GRBEnv env = new GRBEnv("mip1.log");
             env.set(GRB.IntParam.LogToConsole, 0);
-            final GRBModel model = new GRBModel(env);
+            GRBModel model = new GRBModel(env);
 
             Student[] students = studentDAO.getAll();
             Course[] courses = courseDAO.getAll();
             Professor[] professors = professorDAO.getAll();
+            Semester[] semesters = semesterDAO.getAll();
 
-            GRBVar[][][] yijk = new GRBVar[students.length][courses.length][professors.length];
+            GRBVar[][][] yijk = new GRBVar[students.length][courses.length][semesters.length];
             // initialize variables here
+            for (int i = 0; i < students.length; i++) {
+                for (int j = 0; j < courses.length; j++) {
+                    for (int k = 0; k < semesters.length; k++) {
+                        GRBVar grbVar = model.addVar(0, 1, 0.0, GRB.BINARY, "");
+                        yijk[i][j][k] = grbVar;
+                    }
+                }
+            }
+
             GRBVar X = model.addVar(0, GRB.INFINITY, 0, GRB.CONTINUOUS, "X");
             model.update();
+            GRBLinExpr obj = new GRBLinExpr();
+            obj.addTerm(1, X);
+            model.setObjective(obj);
 
             Collection<Constraint> constraints = context.getBeansOfType(Constraint.class).values();
 
