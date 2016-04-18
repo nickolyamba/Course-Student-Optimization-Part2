dad713c78b52ef4cbe0c1f64299916a2cb2c814e
Jack Reed <phillipjreed@gmail.com>
2016-04-17 21:13:18 -0700

    add links to default index page

--- src/main/resources/static/js/course_preferences/index.js
+++ src/main/resources/static/js/course_preferences/index.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     var _this = this;
     var el = $(this);
     var id = el.data().requestId;
+    $('[data-request-id]').removeClass('active');
+    el.addClass('active');
     $.get("course_preferences/" + id).complete(function(data) {
       var html = data.responseText;
       var table = $(html).find('table');
       $('table').replaceWith(table);
-      console.log(table);
     });
   });
 });
