dd0801b8c89c5619475729622c27519e316e77b6
nickolyamba <nickolyamba@yahoo.com>
2016-04-22 12:41:11 -0700

    Added Config class to confgure objectives coefficients from Admin UI using toggle and slider

--- src/main/java/edu/gatech/projectThree/service/Constraint/impl/StudentPriorityObjective.java
+++ src/main/java/edu/gatech/projectThree/service/Constraint/impl/StudentPriorityObjective.java
@@ -7,6 +7,7 @@ import edu.gatech.projectThree.service.Constraint.BaseConstraint;
 import gurobi.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,6 +20,12 @@ import java.util.List;
 @Component
 public class StudentPriorityObjective extends BaseConstraint {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private GlobalState state;
+
+    @Autowired
+    public void setState(GlobalState state) {
+        this.state = state;
+    }
 
     // It would be great to have a slider UI to allow change of the coefficient by an admin
     // via GlobalState, or database entity (preferable)
@@ -37,38 +44,116 @@ public class StudentPriorityObjective extends BaseConstraint {
     //obj.addTerm(CONCENTRATION_K*priority*(SENIORITY_K - seniority.ordinal())*(GPA_K - gpa), prefG[k]);
 
     // Minimization of TAs contribution coefficient
-    private static final double TA_MIN_COEFF = 2.4; // > 2.5 goes to the TA side on custom data sample
+    private double TA_MIN_COEFF = 2.5; //2.4; // > 2.5 goes to the TA side on custom data sample
 
     @Override
     public void constrain(GRBModel model, GRBVar[] prefG, GRBVar[] professorsOfferings, GRBVar[] tasOfferings,
                           List<Student> students, List<Offering> offerings, List<Professor> professors, List<Ta> tas,
                           List<TaOffering> taOfferings, List<ProfessorOffering> profOfferings, List<Preference> preferences)
                           throws GRBException {
-
+        LOGGER.info("Config: " + state.getConfig());
+        TA_MIN_COEFF = state.getConfig().getTaCoeff();
         int priority;
         int k = 0;
         // Create objective expression
         GRBLinExpr obj = new GRBLinExpr();
         Seniority seniority;
         double gpa;
-        for (Preference preference : preferences)
+
+        if (state.getConfig().isGpa() && state.getConfig().isSeniority())
         {
-            priority = preference.getPriority();
-            Student student = preference.getStudent();
+            LOGGER.info("BOTH ARE TRUE");
+            for (Preference preference : preferences)
+            {
+                priority = preference.getPriority();
+                Student student = preference.getStudent();
 
-            seniority = student.getSeniority();
-            gpa = student.getGpa();
+                seniority = student.getSeniority();
+                gpa = student.getGpa();
 
-            Specialization specialization = student.getSpecialization();
-            Course course = preference.getOffering().getCourse();
+                Specialization specialization = student.getSpecialization();
+                Course course = preference.getOffering().getCourse();
 
-            if (specialization.getCourses().contains(course))
-                obj.addTerm(CONCENTRATION_K*priority, prefG[k]);
-            else
-                obj.addTerm(priority, prefG[k]);
-            //LOGGER.info("prefG[" + String.valueOf(preference.getId())+ "] added");
-            k++;
+                if (specialization.getCourses().contains(course))
+                    obj.addTerm(CONCENTRATION_K*priority*(SENIORITY_K - seniority.ordinal())*(GPA_K - gpa), prefG[k]);
+                else
+                    obj.addTerm(priority*(SENIORITY_K - seniority.ordinal())*(GPA_K - gpa), prefG[k]);
+
+                //LOGGER.info("prefG[" + String.valueOf(preference.getId())+ "] added");
+                k++;
+            }//for
+        }//if
+
+        else if(state.getConfig().isGpa())
+        {
+            LOGGER.info("isGpa() is TRUE");
+            for (Preference preference : preferences)
+            {
+                priority = preference.getPriority();
+                Student student = preference.getStudent();
+
+                seniority = student.getSeniority();
+                gpa = student.getGpa();
+
+                Specialization specialization = student.getSpecialization();
+                Course course = preference.getOffering().getCourse();
+
+                if (specialization.getCourses().contains(course))
+                    obj.addTerm(CONCENTRATION_K*priority*(GPA_K - gpa), prefG[k]);
+                else
+                    obj.addTerm(priority*(GPA_K - gpa), prefG[k]);
+
+                //LOGGER.info("prefG[" + String.valueOf(preference.getId())+ "] added");
+                k++;
+            }//for
         }
+
+        else if(state.getConfig().isSeniority())
+        {
+            LOGGER.info("isSeniority() is TRUE");
+            for (Preference preference : preferences)
+            {
+                priority = preference.getPriority();
+                Student student = preference.getStudent();
+
+                seniority = student.getSeniority();
+                gpa = student.getGpa();
+
+                Specialization specialization = student.getSpecialization();
+                Course course = preference.getOffering().getCourse();
+
+                if (specialization.getCourses().contains(course))
+                    obj.addTerm(CONCENTRATION_K*priority*(SENIORITY_K - seniority.ordinal()), prefG[k]);
+                else
+                    obj.addTerm(priority*(SENIORITY_K - seniority.ordinal()), prefG[k]);
+                //LOGGER.info("prefG[" + String.valueOf(preference.getId())+ "] added");
+                k++;
+            }//for
+        }
+
+        // Both False
+        else
+        {   LOGGER.info("BOTH ARE FALSE");
+            for (Preference preference : preferences)
+            {
+                priority = preference.getPriority();
+                Student student = preference.getStudent();
+
+                seniority = student.getSeniority();
+                gpa = student.getGpa();
+
+                Specialization specialization = student.getSpecialization();
+                Course course = preference.getOffering().getCourse();
+
+                if (specialization.getCourses().contains(course))
+                    obj.addTerm(CONCENTRATION_K*priority, prefG[k]);
+                else
+                    obj.addTerm(priority, prefG[k]);
+
+                //LOGGER.info("prefG[" + String.valueOf(preference.getId())+ "] added");
+                k++;
+            }//for
+        }//else
 
 
         // Minimize number of TAs
